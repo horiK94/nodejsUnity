0eae56a21803b70b557d741abd186e1a49f35491
hamity <hazemi94@gmail.com>
2019-07-02 23:41:12 +0900

    色々試してみた

--- NodejsSample/Assets/ClientExample.cs
+++ NodejsSample/Assets/ClientExample.cs
@@ -7,13 +7,26 @@ public class ClientExample : MonoBehaviour {
     private WebSocket ws;
     private Vector3 beforePos = Vector3.zero;
     
+    private int playerId = 0;
+    
     void OnGUI() {
         if (GUILayout.Button("Connect")) {
             this.ws = new WebSocket("ws://127.0.0.1:8080");
             this.ws.OnMessage += (object sender, MessageEventArgs e) => {
-                print (e.Data);
+                Debug.Log("Send");
             };
             this.ws.Connect ();
+
+            //データ受け取り時の処理
+            this.ws.OnMessage += (sender, e) => {
+                if(playerId == 0)
+                {
+                    //プレイヤーidが向こう値の場合はidが送られることを期待する
+                    PlayerPosition jsonData = JsonUtility.FromJson<PlayerPosition>(e.Data);
+                    playerId = jsonData.id;
+                    Debug.Log(playerId);
+                }
+            };
         }
         
         /*
@@ -46,17 +59,17 @@ public class ClientExample : MonoBehaviour {
             beforePos = worldPos;
 
             PlayerPosition playerPosition = new PlayerPosition();
-            playerPosition.playerId = 0;
+            playerPosition.id = playerId;
             playerPosition.position = worldPos;
             playerPosition.date = DateTime.Now;
-            this.ws.Send(JsonUtility.ToJson(playerPosition));
+            //this.ws.Send(JsonUtility.ToJson(playerPosition));
         }
     }
 }
 
 struct PlayerPosition
 {
-    public int playerId;
+    public int id;
     public Vector3 position;
     public DateTime date;
 }
--- server.js
+++ server.js
@@ -3,9 +3,13 @@ var wss = new WebSocketServer({
   port: 8080
 });
 
+let playerId = 1;
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     console.log('received: %s', message);
   });
-  ws.send('something');
+  let sendData = {};
+  sendData["id"] = playerId;
+  playerId++;
+  ws.send(JSON.stringify(sendData));
 });
